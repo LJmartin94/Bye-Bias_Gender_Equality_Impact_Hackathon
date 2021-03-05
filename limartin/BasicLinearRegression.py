7b6cc3177d6e1736e38dc21345315ddff4ccb90d
limartin <lindsayjamesmartin94@gmail.com>
2021-03-05 12:52:41 +0100

    Converted YearsCodingProf to int based on maarten's function

--- limartin/BasicLinearRegression.py
+++ limartin/BasicLinearRegression.py
@@ -11,78 +11,66 @@ import matplotlib.pyplot as plt
 
 
 # load data set
-data = pd.read_csv('./50_Startups.csv')
-# data = pd.read_csv('./megainc.csv')
+#data = pd.read_csv('./50_Startups.csv')
+df = pd.read_csv('../megainc.csv')
 
-# X = data[['Gender', 'YearsCodingProf']]
-# y = data['ConvertedSalary']
+df = df.loc[df['ConvertedSalary'] < 300000] # Only select convertedsalary of less than 250,000
+df = df.dropna()
 
-# regr = LinearRegression()
-# regr.fit(X, y)
+def replace_yearscodingprof(df):
+    df = df.replace(to_replace = ['0-2 years'], value = 1)
+    df = df.replace(to_replace = ['3-5 years'], value = 4)
+    df = df.replace(to_replace = ['6-8 years'], value = 7)
+    df = df.replace(to_replace = ['9-11 years'], value = 10)
+    df = df.replace(to_replace = ['12-14 years'], value = 13)
+    df = df.replace(to_replace = ['15-17 years'], value = 16)
+    df = df.replace(to_replace = ['18-20 years'], value = 19)
+    df = df.replace(to_replace = ['21-23 years'], value = 22)
+    df = df.replace(to_replace = ['24-26 years'], value = 25)
+    df = df.replace(to_replace = ['27-29 years'], value = 28)
+    df = df.replace(to_replace = ['30 or more years'], value = 30)
+    return df
 
-
+df = replace_yearscodingprof(df)
 
 # values converts it into a numpy array: -1 means auto interpret number of rows, 1 means me want 1 column
-X = data.iloc[:, :-1].values #.reshape(-1, 1)  
-Y = data.iloc[:, 4].values #.reshape(-1, 1)
+X = df.loc[:, 'YearsCodingProf'].values.reshape(-1, 1)  
+Y = df.loc[:, 'ConvertedSalary'].values.reshape(-1, 1)
+
+linear_regressor = LinearRegression()  # create object for the class
+linear_regressor.fit(X, Y)  # perform linear regression
+Y_pred = linear_regressor.predict(X)  # make predictions
+
+df.plot(kind='scatter', x='YearsCodingProf', y='ConvertedSalary')
+plt.plot(X, Y_pred, color='red')
+plt.show()
+print(linear_regressor.coef_)
 
 
-#Convert the column into categorical columns
-states=pd.get_dummies(X['State'],drop_first=True)
-# Drop the state coulmn
-X=X.drop('State',axis=1)
-# concat the dummy variables
-X=pd.concat([X,states],axis=1)
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
-# Fitting Multiple Linear Regression to the Training set
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
-# Predicting the Test set results
-y_pred = regressor.predict(X_test)
-from sklearn.metrics import r2_score
-score=r2_score(y_test,y_pred)
+# #Convert the column into categorical columns
+# states=pd.get_dummies(X['State'],drop_first=True)
+# # Drop the state coulmn
+# X=X.drop('State',axis=1)
+# # concat the dummy variables
+# X=pd.concat([X,states],axis=1)
+# # Splitting the dataset into the Training set and Test set
+# from sklearn.model_selection import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+# # Fitting Multiple Linear Regression to the Training set
+# from sklearn.linear_model import LinearRegression
+# regressor = LinearRegression()
+# regressor.fit(X_train, y_train)
+# # Predicting the Test set results
+# y_pred = regressor.predict(X_test)
+# from sklearn.metrics import r2_score
+# score=r2_score(y_test,y_pred)
 
 
-print(regr.coef_)
-
-
-
-
-
-
-
-
-
-
-
-
+# print(regr.coef_)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# linear_regressor = LinearRegression()  # create object for the class
-# linear_regressor.fit(X, Y)  # perform linear regression
-# Y_pred = linear_regressor.predict(X)  # make predictions
-
-# data.plot(kind='scatter', x='R&D Spend', y='Profit')
-# plt.plot(X, Y_pred, color='red')
-# plt.show()
 
 # labelencoder = LabelEncoder()
 # X[:,3] = labelenencoder.fit_transform(X[:,3])
@@ -102,6 +90,8 @@ print(regr.coef_)
 # regressor.fit(X_train, y_train)
 # y_pred = regressor.predict(X_test)
 
+
+##Dropping variables based on p value
 # # X = np.append(arr = np.ones((30,1)).astype(int), values = X, axis = 1)
 # # X_opt = X[:,[0,1,2,3,4,5]]
 # # regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
@@ -120,6 +110,6 @@ print(regr.coef_)
 
 
 
-# # data.plot(kind='scatter', x='R&D Spend', y='Profit')
-# # plt.plot(X_test, y_pred, color='red')
-# # plt.show()
+# data.plot(kind='scatter', x='R&D Spend', y='Profit')
+# plt.plot(X_test, y_pred, color='red')
+# plt.show()
